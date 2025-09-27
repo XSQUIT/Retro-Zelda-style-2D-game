ecd58e5f733d6834d0a1902362a3dd146c71f1df
XSQUIT <qurdidozelda202@gmail.com>
2025-09-27 11:48:02 +0200

    added: -Main class -GamePanel class Keyhandler class -Player class -Entity class

---
+++ src/entity/Player.java
@@ -0,0 +1,41 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import java.awt.*;
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler keyH;
+
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+        setDefaultvalues();
+    }
+    public void setDefaultvalues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+    }
+    public void update() {
+        if(keyH.upPressed) {
+            y -= speed;
+        }
+        else if (keyH.downPressed) {
+            y += speed;
+        }
+        else if (keyH.rightPressed) {
+            x += speed;
+        }
+        else if (keyH.leftPressed) {
+            x -= speed;
+        }
+    }
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.cyan);
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+    }
+}
